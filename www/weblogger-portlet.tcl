eb29484b6f74a406f6fbb438f2c4522fd1fb27f7
Miguel Cordova <mac@innova.uned.es>
2008-11-11 13:22:32 +0000

    tabs2spaces and reindentation

--- www/weblogger-portlet.tcl
+++ www/weblogger-portlet.tcl
@@ -11,14 +11,20 @@ ad_page_contract {
     @author Simon Carstensen (simon@bcuni.net)
 } {
     item_id:integer,notnull,optional,multiple
+    length:integer,optional
 } -properties {
 
+}
+
+# Default value; TODO get from package paramete
+if  { ![info exists length]} {
+    set length 100
 }
 
 set user_id [ad_conn user_id]
 set write_p [ad_permission_p $user_id admin]
 
-array set config $cf	
+array set config $cf    
 set shaded_p $config(shaded_p)
 
 set list_of_package_ids $config(package_id)
@@ -40,23 +46,20 @@ template::list::create -name blogs -multirow entries -key entry_id -pass_propert
     item {
         label ""
         display_template {
-          <b>@entries.parent_name@</b></br>
+          <p class="blog-name"><b>@entries.parent_name@</b>
+            <small><if @entries.add_url@ not nil><a class="button" href="@entries.add_url@">#weblogger-portlet.Add_entry#</a>
+             </if>
+            <a class="button" href="@entries.base_url@">\#weblogger-portlet.FullView\#</a></small>
+          </p>
+        <ul>
           <group column="package_id">
             <if @entries.entry_id@ not nil>
-	    &raquo; <i><a href="@entries.view_url@" title="View this entry">@entries.title@</a></i> 
-	            @entries.content;noquote@ 
-                    <small>(@entries.posted_time_pretty@)</small></br>
+            <li class="blog-entry"> <i><a href="@entries.view_url@" title="View this entry">@entries.title@</a></i> <small>(@entries.posted_time_pretty@)</small>
+              <br><span class="blog-content">@entries.content;noquote@</span> 
+            </li>
             </if>
-          </group>
+          </group></ul>
         }
-    }
-    actions {
-	label ""
-	display_template {
-            <if @entries.add_url@ not nil><a href="@entries.add_url@">#weblogger-portlet.Add_entry#</a>
-            | </if>
-            <a href="@entries.base_url@">\#weblogger-portlet.FullView\#</a>
-	}
     }
 }
 
@@ -64,7 +67,7 @@ db_multirow -extend { view_url add_url } entries entries {} {
     if { !$package_read_p($package_id) } {
         continue
     }
-    set content [string_truncate -len 100 -- $content]
+    set content [string_truncate -len $length -- $content]
     set view_url "${base_url}one-entry?[export_vars { entry_id }]"
     if { $package_create_p($package_id) } {
         set add_url "${base_url}entry-edit"
